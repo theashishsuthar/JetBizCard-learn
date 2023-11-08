de9bf4078d2871cc161317590ebb39fe7437d4e3
Glasierinc <glasier.inc@gmail.com>
2023-11-08 18:08:55 +0530

    Profile image loaded

--- app/src/main/java/com/example/jetbizcard/MainActivity.kt
+++ app/src/main/java/com/example/jetbizcard/MainActivity.kt
@@ -3,21 +3,32 @@ package com.example.jetbizcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetbizcard.ui.theme.JetBizCardTheme
@@ -29,10 +40,9 @@ class MainActivity : ComponentActivity() {
             JetBizCardTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    CreateBizCard()
+                    CreateBizCard("Hey! There")
                 }
             }
         }
@@ -40,29 +50,54 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun CreateBizCard(){
-    Surface (modifier = Modifier
-        .fillMaxWidth()
-        .fillMaxHeight()) {
-        Card (modifier= Modifier
+fun CreateBizCard(abc: String) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        Card(modifier = Modifier
             .width(200.dp)
             .height(390.dp)
-            .padding(12.dp), elevation = CardDefaults.cardElevation(
+            .padding(12.dp),
+            elevation = CardDefaults.cardElevation(
                 2.dp
-            ) ,shape = RoundedCornerShape(corner = CornerSize(12.dp)), colors = CardDefaults.cardColors(
+            ),
+            shape = RoundedCornerShape(corner = CornerSize(20.dp)),
+            colors = CardDefaults.cardColors(
                 containerColor = Color.White
-            ), content = {
+            ),
+            content = {
+                Surface(
+                    modifier = Modifier
+                        .size(150.dp)
+                        .padding(5.dp),
+                    color = MaterialTheme.colorScheme.surface.copy(alpha = 0.5f),
+                    shadowElevation = 2.dp,
+                    shape = RoundedCornerShape(30.dp)
+                ) {
 
-        })
+                    Image(
+                        modifier = Modifier
+                            .height(20.dp)
+                            .width(30.dp)
+                            .padding(5.dp)
+                            .background(color = MaterialTheme.colorScheme.surface.copy(alpha = 0.5f)),
+                        painter = painterResource(id = R.drawable.wellness_logo),
+                        contentDescription = "wellness logo",
+                        contentScale = ContentScale.Crop,
+                    )
+
+                }
+            })
     }
 }
-
 
 
 @Preview(showBackground = true)
 @Composable
 fun BizCardPreview() {
     JetBizCardTheme {
-CreateBizCard()
+        CreateBizCard("Hye! How are you ?")
     }
 }
